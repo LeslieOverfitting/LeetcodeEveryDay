cf82118e47f346eb921645f3a658713735627052
wfs <296526299@qq.com>
2021-08-08 15:44:18 +0800

    num 70

---
+++ 70.爬楼梯.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+/*
+ * @lc app=leetcode.cn id=70 lang=cpp
+ *
+ * [70] 爬楼梯
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n + 3, 0);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3; i <=n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+
+    }
+};
+// @lc code=end
+
